8f6f0e22f2edd8bfcd3605dcb8440474f4e1a726
ac8274 <ac8274@bs.amalnet.k12.il>
2022-12-26 20:50:45 +0200

    hello

---
+++ app/src/main/java/com/example/buttten4/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.buttten4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.btn);
+    }
+
+    public void go(View view) {
+        String hello = "Yea i've been clicked!";
+        btn.setText(hello);
+    }
+}
